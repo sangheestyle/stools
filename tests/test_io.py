9c3efbc7e3f3ad0de72ec2550b700caff714ff3d
Sanghee Kim <sangheestyle@gmail.com>
2014-02-03 18:32:03 -0700

    test read_json_folder

---
+++ tests/test_io.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from stools import io
+
+class TestIO(unittest.TestCase):
+
+    def setUp(self):
+        self.json_folder_path = "data"
+        self.ends_with = ".json"
+
+    def test_json(self):
+        json_list = io.read_json_folder(self.json_folder_path, self.ends_with)
+        num_json_files = 0
+        for root, folder, names in os.walk(self.json_folder_path):
+            for name in names:
+                if name.endswith('.json'):
+                    num_json_files += 1
+        self.assertEqual(type(json_list), type(list()))
+        self.assertEqual(len(json_list), num_json_files)
+
+if __name__=='__main__':
+    unittest.main()
