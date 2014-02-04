ca96dc5ab38a49cabf7b3a1c718d057392d3048f
Sanghee Kim <sangheestyle@gmail.com>
2014-02-03 19:55:36 -0700

    filtering names in json and returning filtered values only into list

--- stools/io.py
+++ stools/io.py
@@ -1,15 +1,28 @@
 import os
 import json
 
-def read_json(path):
-    with open(path) as fp:
-        return json.load(fp)
+def json_filter(json_content, names):
+    filtered = []
+    for name in names:
+        value = json_content
+        for item in name:
+            value = value.get(item)
+        filtered.append(value)
+    return filtered
 
-def read_json_folder(folder_path=None, ends_with='.json'):
+def read_json(path=None, names=None):
+    with open(path) as fp:
+        json_content = json.load(fp)
+    if names == None:
+        return json_content
+    else:
+        return json_filter(json_content, names)
+
+def read_json_folder(folder_path=None, ends_with='.json', names=None):
     json_list = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith(ends_with):
-                json_content = read_json(os.path.join(root, file))
+                json_content = read_json(os.path.join(root, file), names)
                 json_list.append(json_content)
     return json_list
--- tests/test_io.py
+++ tests/test_io.py
@@ -9,12 +9,14 @@ class TestIO(unittest.TestCase):
         self.ends_with = ".json"
 
     def test_json(self):
-        json_list = io.read_json_folder(self.json_folder_path, self.ends_with)
+        json_names = [['category'],['extendedInfo','description']]
+        json_list = io.read_json_folder(self.json_folder_path, self.ends_with, json_names)
         num_json_files = 0
         for root, folder, names in os.walk(self.json_folder_path):
             for name in names:
                 if name.endswith('.json'):
                     num_json_files += 1
+        print json_list
         self.assertEqual(type(json_list), type(list()))
         self.assertEqual(len(json_list), num_json_files)
 
